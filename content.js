2899966e89b4aabcf9b31a63270113ee9f834afa
Tim Spiering <t.spiering@cag.nl>
2019-07-08 12:03:07 +0200

    First working version of downloading tables as pdf

--- content.js
+++ content.js
@@ -1,9 +1,25 @@
 window.onload = function() {
 	chrome.runtime.onMessage.addListener(function(msg) {
-		console.log(msg);
+	    if (msg.msg == "start") {
+	    	var data = [];
+			var tables = document.getElementsByTagName('table');
 
-	    if (test.test == "test") {
-	    	alert("Works");
+	    	for (i = 1; i <= 4; i++) {
+	    		data.push(tables[i].innerHTML);
+	    	}
+
+		    var doc = new jsPDF();
+
+		    doc.fromHTML(
+		    	"<table>" + data[2] + "</table>",
+		    	1,
+		    	1,
+		    	{
+		    		'width': 180
+		    	}
+			);
+
+			doc.save('doc.pdf');
 	    }
 	});
 }
